50c17780c82a4e27bfabad28b7992b0ac3e38c9c
Diego Ferreira <diferreira.program@gmail.com>
2022-01-12 17:01:00 -0300

    create route products GET, POST

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,48 @@
 import { Router } from "express";
+import { getManager, getRepository } from "typeorm";
+import { Products } from "../database/models/Products";
 
 const productsRoutes = Router();
 
-productsRoutes.get("/", async (req, res) => {
-  res.send("Rota de cardapio");
+productsRoutes.get("/", async (request, response) => {
+  const res = await getRepository(Products).find();
+  response.json(res);
+});
+
+productsRoutes.post("/", async (request, response) => {
+  try {
+    const manager = getManager();
+
+    const product = manager.create(Products);
+    product.name = request.body.name;
+    product.price = request.body.price;
+    request.body.description
+      ? (product.description = request.body.description)
+      : null;
+
+    const res = await manager.save(product);
+
+    return response.status(201).json(res);
+  } catch (err) {
+    console.error("Produto router error =>", err.message);
+  }
+});
+
+productsRoutes.get("/:id", async (request, response) => {
+  const res = await getRepository(Products).find({
+    where: {
+      id: request.params.id,
+    },
+  });
+  response.json(res);
+});
+
+productsRoutes.put("/:id", async (request, response) => {
+  response.send("Rota de cardapio");
+});
+
+productsRoutes.delete("/:id", async (request, response) => {
+  response.send("Rota de cardapio");
 });
 
 export default productsRoutes;
