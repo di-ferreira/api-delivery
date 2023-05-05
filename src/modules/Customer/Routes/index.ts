ae2c768bdbfeae1ad5898b44dbe55761738994ad
Diego Ferreira <diferreira.program@gmail.com>
2023-05-04 21:35:57 -0300

    add package celebrate

--- src/modules/Customer/Routes/index.ts
+++ src/modules/Customer/Routes/index.ts
@@ -1,3 +1,4 @@
+import { Joi, Segments, celebrate } from 'celebrate';
 import { Router } from 'express';
 import CustomerController from '../CustomerController';
 
@@ -5,10 +6,37 @@ const Controller = new CustomerController();
 
 const CustomerRoute = Router();
 
-CustomerRoute.post('/', Controller.create);
+CustomerRoute.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      phone: Joi.string().required(),
+    },
+  }),
+  Controller.create
+);
 CustomerRoute.get('/', Controller.index);
-CustomerRoute.get('/:id', Controller.show);
-CustomerRoute.put('/:id', Controller.update);
-CustomerRoute.delete('/:id', Controller.delete);
+CustomerRoute.get(
+  '/:id',
+  celebrate({ [Segments.PARAMS]: { id: Joi.string().id().required() } }),
+  Controller.show
+);
+CustomerRoute.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      phone: Joi.string().required(),
+    },
+    [Segments.PARAMS]: { id: Joi.string().id().required() },
+  }),
+  Controller.update
+);
+CustomerRoute.delete(
+  '/:id',
+  celebrate({ [Segments.PARAMS]: { id: Joi.string().id().required() } }),
+  Controller.delete
+);
 
 export default CustomerRoute;
